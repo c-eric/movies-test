7573792f955d4af9211c3774d89688f27ccbc376
c-eric <43305167+c-eric@users.noreply.github.com>
2024-02-17 23:34:02 -0500

    Initial commit - backend

---
+++ src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,33 @@
+package com.example.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    // treat this property as the unique identifier
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+    // database only store the ids of the review, reviews will be in a separate collection
+    // https://www.mongodb.com/docs/manual/tutorial/model-referenced-one-to-many-relationships-between-documents/
+    @DocumentReference
+    private List<Review> reviewIds;
+}
